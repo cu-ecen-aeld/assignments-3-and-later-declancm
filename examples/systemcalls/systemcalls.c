624ea03ff4ed265effea1a223f8345ff548283aa
Declan Mullen <declanmullen3.14@gmail.com>
2025-02-04 02:16:54 -0800

    Implemented system calls

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,13 +15,18 @@
 */
 bool do_system(const char *cmd)
 {
+    int ret = system(cmd);
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    if (ret == -1)
+    {
+        perror("system() failed");
+        return false;
+    }
+    else if (ret != 0)
+    {
+        fprintf(stderr, "Command failed with return code %d\n", ret);
+        return false;
+    }
 
     return true;
 }
@@ -45,19 +56,28 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    pid_t pid;
+
+    switch (pid = fork()) {
+        case -1:
+            perror("fork() failed");
+            return false;
+        case 0:
+            execv(command[0], command);
+            perror("execv() failed");
+            exit(1);
+        default: {
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+            {
+                fprintf(stderr, "Command failed with return code %d\n", WEXITSTATUS(status));
+                return false;
+            }
+        }
+    }
 
     va_end(args);
 
@@ -80,18 +100,43 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    pid_t pid;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    if (fd < 0)
+    {
+        perror("open() failed");
+        return false;
+    }
+
+    switch (pid = fork()) {
+        case -1:
+            perror("fork() failed");
+            return false;
+        case 0:
+            if (dup2(fd, 1) < 0)
+            {
+                perror("dup2() failed");
+                close(fd);
+                exit(1);
+            }
+
+            close(fd);
+            execv(command[0], command);
+            perror("execv() failed");
+            exit(1);
+        default:
+            close(fd);
+            int status;
+            waitpid(pid, &status, 0);
+
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+            {
+                fprintf(stderr, "Command failed with return code %d\n", WEXITSTATUS(status));
+                return false;
+            }
+    }
 
     va_end(args);
 
