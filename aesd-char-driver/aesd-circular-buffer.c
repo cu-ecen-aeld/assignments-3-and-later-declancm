3c138614e572eedd9b4ee17503542452589d29aa
Declan Mullen <declanmullen3.14@gmail.com>
2025-02-20 21:13:57 -0800

    Return pointer to overwritten circular buffer entries

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -58,9 +58,12 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * new start location.
 * Any necessary locking must be handled by the caller
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
+* Returns a pointer to any overwritten entry, or NULL if no entry was overwritten.
 */
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+struct aesd_buffer_entry *aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+    struct aesd_buffer_entry *overwritten_entry = &buffer->entry[buffer->out_offs];
+
     if (buffer->full)
     {
         buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
@@ -77,6 +80,8 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     {
         buffer->full = false;
     }
+
+    return buffer->full ? overwritten_entry : NULL;
 }
 
 /**
